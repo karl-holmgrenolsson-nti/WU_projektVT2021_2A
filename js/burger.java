776d548193d2f27f3dde490e9a3ab923c7a4c0c4
karl-holmgrenolsson-nti-johanneberg <74712711+karl-holmgrenolsson-nti@users.noreply.github.com>
2021-03-12 16:07:07 +0100

    Java
    Idag har jag fixat med Java så att det kan användas när jag blir klar med hemsidans struktur.

--- js/burger.java
+++ js/burger.java
@@ -1,1 +1,12 @@
-I denna mapp placeras js-dokument.
+function Togglemenu(){
+
+    document.query Selecter(:nav-links:) classlist.toggle('nav-active')
+
+    document.query('burger').classlist.toggle()
+}
+
+document.query('burger').addEventlistner('We toggle menu')
+
+cunst:nav = document.queryselector('burger')('toggle')
+
+burger.addEventlistner
